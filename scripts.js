33ca148e48329e656f4921da81aecd7f58a49903
Bela Kristof <belakristof484@gmail.com>
2025-04-16 11:07:46 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,13 +13,13 @@ const startButton = document.querySelector(".start-btn");
       // Generate 9 holes and moles
       for (let i = 0; i < 9; i++) {
         const hole = document.createElement("img");
-        hole.src = "images/hole3.png";
+        hole.src = "hole3.png";
         hole.className = "holes hole" + (i + 1);
         hole.style.top = `${350 + Math.floor(i / 3) * 70}px`;
         hole.style.left = `${700 + (i % 3) * 120}px`;
 
         const mole = document.createElement("img");
-        mole.src = "images/mole1.png";
+        mole.src = "mole1.png";
         mole.className = "moles mole" + (i + 1);
         mole.style.top = `${340 + Math.floor(i / 3) * 70}px`;
         mole.style.left = `${730 + (i % 3) * 120}px`;
